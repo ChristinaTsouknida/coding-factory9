d4b3f870af44e61fa830a6c8b936143e08027b73
Christina Tsouknida <christina.tsouknida@gmail.com>
2025-12-08 20:04:24 +0200

    feat: add Utility class

---
+++ src/gr/aueb/cf/cf9/ch13/HelloUtil.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.cf9.ch13;
+
+/**
+ * Utility classes are non-instantiable.
+ * They provide public static methods only.
+ */
+public class HelloUtil {
+
+    private HelloUtil() {
+
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello Coding!!");
+    }
+}
