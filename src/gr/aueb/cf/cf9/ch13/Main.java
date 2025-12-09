0065e68414610ebde373df248e48243c20a6254c
Christina Tsouknida <christina.tsouknida@gmail.com>
2025-12-09 18:49:12 +0200

    feat: fix main

--- src/gr/aueb/cf/cf9/ch13/Main.java
+++ src/gr/aueb/cf/cf9/ch13/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         HelloUtil.sayHello();
+
+        OnlyOneCodingFactory onlyOneCodingFactory1 = OnlyOneCodingFactory.getInstance();
+        OnlyOneCodingFactory onlyOneCodingFactory2 = OnlyOneCodingFactory.getInstance();
+
+        OnlyOneCodingLazy onlyOneCodingLazy = OnlyOneCodingLazy.getInstance();
+        OnlyOneCodingLazy onlyOneCodingLazy2 = OnlyOneCodingLazy.getInstance();
     }
 }
