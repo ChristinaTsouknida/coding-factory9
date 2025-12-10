4394a167c0f62cccef1f6c87e2efc85a57cb210c
Christina Tsouknida <christina.tsouknida@gmail.com>
2025-12-10 19:46:44 +0200

    fix: add product changes

--- src/gr/aueb/cf/cf9/ch15/marker/Product.java
+++ src/gr/aueb/cf/cf9/ch15/marker/Product.java
@@ -6,5 +6,28 @@ public class Product {
     private int id;
     private String title;
 
-    public gr.
+    public Product() {
+
+    }
+
+    public Product(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
